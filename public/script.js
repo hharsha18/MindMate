35e157ca1e41d23f1140443b12d887ef1fd83f93
Harsha <hhharsha2006@gmail.com>
2026-05-24 19:00:49 +0530

    Fixed deployment API route

--- public/script.js
+++ public/script.js
@@ -31,7 +31,7 @@ async function sendMessage() {
     try {
 
         const response =
-            await fetch("http://localhost:3000/chat", {
+            await fetch("/chat", {
 
             method: "POST",
 
